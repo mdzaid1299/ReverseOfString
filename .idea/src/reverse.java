a7677cb30e802c2a12f903164b1a67aac5cf6528
HP <mdzaid1299@gmail.com>
2022-11-11 21:26:50 +0530

    ini

--- .idea/src/reverse.java
+++ .idea/src/reverse.java
@@ -1,3 +1,15 @@
-public class reverse {
+import java.util.Scanner;
 
+public class reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a String");
+        String userIn  = scanner.nextLine();
+        String string = "";
+        for (int i = 0; i < userIn.length(); i++) {
+            userIn[1] = string;
+            string =
+        }
+    }
 }
+
